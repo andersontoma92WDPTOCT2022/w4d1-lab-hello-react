cda78a8b93b9d2bfa80b9908edb0c43e8363ac1d
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-11-07 16:42:56 -0300

    imagens e texto incluidos

--- lab-hello-react/src/App.js
+++ lab-hello-react/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import CardInfo from './components/CardInfo';
 import SayHello from './components/SayHello';
 //import CardInfo from './components/CardInfo';
 //import Ironlogo from '../src/images/ironhack-logo-xs.png';
@@ -19,23 +20,45 @@ function App() {
     {
       end: 'https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon1.png',
       alt: 'img1',
+      titulo: 'Declarative',
+      paragrafo: 'xxx xgjl fsaj fsaoj  gg pojgoa go ',
     },
     {
       end: 'https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon2.png',
       alt: 'img2',
+      titulo: 'Components',
+      paragrafo: ' gfasf skapf kspaokfg paokgpoa apf kspaokfg  apf kspaokfg ',
     },
     {
       end: 'https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon3.png',
       alt: 'img3',
+      titulo: 'Single-way',
+      paragrafo:
+        'fops fpak pgfoaks gfpakpgfo p fpak pgfoaks gfpfpak pgfoaks gfp fpak pgfoaks gfp',
+    },
+    {
+      end: 'https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/icon4.png',
+      alt: 'img4',
+      titulo: 'JSX',
+      paragrafo:
+        'pofpokojgf p  pso kpaofpa pa pk poetp powpoje oijefoi wjaojawoiefj oawjfoiajwojaw fpak pgfoaks gfp',
     },
   ];
   //----------------------------------------
   return (
     <div className="App">
-      <img src={imagens1[0].end} alt="imagem educação" className="back" />
-      <img src={imagens1[1].end} alt="imagem educação" className="back" />
+      <img src={imagens1[0].end} alt={imagens1[1].alt} className="back" />
+      <img src={imagens1[1].end} alt={imagens1[1].alt} className="back" />
       <SayHello />
-      {imagens2}.
+      {imagens2.map((cartao) => (
+        <CardInfo
+          end={cartao.end}
+          alt={cartao.alt}
+          titulo={cartao.titulo}
+          paragrafo={cartao.paragrafo}
+        />
+      ))}
+      <CardInfo end={imagens2[0].end} alt={imagens2[0].alt} />
     </div>
   );
 }
--- lab-hello-react/src/components/CardInfo.js
+++ lab-hello-react/src/components/CardInfo.js
@@ -1,9 +1,14 @@
 function CardInfo(props) {
   //
+  console.log(props, 'props');
   return (
     <div>
       {/**/}
       <img src={props.end} alt={props.alt} />
+      <div>
+        <h3>{props.titulo}</h3>
+        <p>{props.paragrafo}</p>
+      </div>
     </div>
   );
 }
